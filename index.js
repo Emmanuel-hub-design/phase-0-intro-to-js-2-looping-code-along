11381264ddd2f300a0cb17adf34ae76f98f828b5
Emmanuel Baraka <barakaemmanuel413@gmail.com>
2023-10-07 04:17:13 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+//code your solutions in this file
+
+function writeCards(names, occasion) {
+  // Create a new empty array to hold the messages
+  const messages = ["Charlie", "Samip", "Ali",];
+
+  // Iterate through the input array
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+
+    // Build the 'thank you' message using string interpolation
+    const message = `Thank you, ${name}, for the wonderful ${occasion} gift!`;debugger;
+
+    // Add the message to the new array
+    messages.push(message);
+  }
+
+  // Return the new array
+  return messages;
+  writeCards(["Charlie", "Samip", "Ali"], "Birthday");
+}
+
+// Example usage:
+const cards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(cards);
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+// Example usage:
+countDown(10);
+
